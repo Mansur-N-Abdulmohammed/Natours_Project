012d5f46d7c3408864bfedbab1bc3aaf8acde1e3
MansurSharazoori <Xpert.Web.911@gmail.com>
2025-09-17 07:22:53 +0300

    navbar

--- main/javascript/javalin.js
+++ main/javascript/javalin.js
@@ -1,6 +1,16 @@
 var btn = document.querySelector(".menu-btn");
 var back = document.querySelector(".green-back");
+var list = document.querySelector(".menu-list");
+var item = document.querySelectorAll(".menu-item-btn");
 
 btn.addEventListener("click", function () {
   back.classList.toggle("open");
+  list.classList.toggle("menu-list-open");
+});
+
+item.forEach(function (I) {
+  I.addEventListener("click", function () {
+    back.classList.toggle("open");
+    list.classList.toggle("menu-list-open");
+  });
 });
